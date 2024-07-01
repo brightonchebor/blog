be6a1857d062cba24968719bfc24298107c8bc6b
Chebor <cheborbrighton805@gmail.com>
2024-07-01 15:52:49 +0300

    updated

--- blog/settings.py
+++ blog/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.stat))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -121,18 +122,20 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
-import os
+
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+"""
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
