d96fc1a5cdbadff1382cef31b77febfa85e2fde8
Chebor <cheborbrighton805@gmail.com>
2024-07-08 15:17:23 +0300

    updated

--- myapp/models.py
+++ myapp/models.py
@@ -67,4 +67,5 @@ class Comment(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '%s - %s' % (self.post.title, self.name)    
+        return '%s - %s' % (self.post.title, self.name)
+    
